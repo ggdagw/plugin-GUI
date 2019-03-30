1012961469f99b49a6076e67a6023fd4ade12a4a
Gethin Williams <gethin.williams@bristol.ac.uk>
2019-03-30 09:33:43 +0000

    high and low thresholds with sample counting and timeout

---
+++ Source/Plugins/LatencyThreshold/LatencyThresholdProcessor.cpp
@@ -0,0 +1,239 @@
+/*
+    ------------------------------------------------------------------
+
+    This file is part of the Open Ephys GUI
+    Copyright (C) 2016 Open Ephys
+
+    ------------------------------------------------------------------
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+#include <stdio.h>
+
+#include "LatencyThresholdProcessor.h"
+#include "LatencyThresholdProcessorEditor.h"
+
+//If the processor uses a custom editor, it needs its header to instantiate it
+//#include "ExampleEditor.h"
+
+
+LatencyThresholdProcessor::LatencyThresholdProcessor()
+  : GenericProcessor ("LatencyThreshold"), highThreshold(5000.0), lowThreshold(230.0), counting(false), sampleCount(0)
+
+{
+    setProcessorType (PROCESSOR_TYPE_FILTER);
+
+    // Open Ephys Plugin Generator will insert generated code for parameters here. Don't edit this section.
+    //[OPENEPHYS_PARAMETERS_SECTION_BEGIN]
+    // Parameter 0 code
+    auto parameter0 = new Parameter ("HighThreshold", "HighThreshold", 0, 6000, 5000, 0);
+    parameters.add (parameter0);
+
+    // Parameter 1 code
+    auto parameter1 = new Parameter ("LowThreshold", "LowThreshold", 0, 2000, 230, 1);
+    parameters.add (parameter1);
+
+
+    //[OPENEPHYS_PARAMETERS_SECTION_END]
+
+    //Without a custom editor, generic parameter controls can be added
+    //parameters.add(Parameter("thresh", 0.0, 500.0, 200.0, 0));
+}
+
+
+LatencyThresholdProcessor::~LatencyThresholdProcessor()
+{
+}
+
+
+/**
+  If the processor uses a custom editor, this method must be present.
+*/
+AudioProcessorEditor* LatencyThresholdProcessor::createEditor()
+{
+    editor = new LatencyThresholdProcessorEditor (this, true);
+
+    //std::cout << "Creating editor." << std::endl;
+
+    return editor;
+}
+
+
+void LatencyThresholdProcessor::setParameter (int parameterIndex, float newValue)
+{
+    GenericProcessor::setParameter (parameterIndex, newValue);
+    editor->updateParameterButtons (parameterIndex);
+
+    //Parameter& p =  parameters.getReference(parameterIndex);
+    //p.setValue(newValue, 0);
+
+    //threshold = newValue;
+
+    //std::cout << float(p[0]) << std::endl;
+
+
+    std::cout << "parameterIndex: " << parameterIndex << std::endl;
+    std::cout << "old value for highThreshold: " << highThreshold << std::endl;
+    std::cout << "old value for lowThreshold: " << lowThreshold << std::endl;
+    if (parameterIndex == 0)
+      highThreshold = newValue;
+    else if (parameterIndex == 1)
+      lowThreshold = newValue;
+    std::cout << "old value for highThreshold: " << highThreshold << std::endl;
+    std::cout << "old value for lowThreshold: " << lowThreshold << std::endl;
+    
+}
+
+
+void LatencyThresholdProcessor::process (AudioSampleBuffer& buffer)
+{
+    /**
+      Generic structure for processing buffer data
+    */
+
+  const int timeOut = 10000; 
+  
+    const int nChannels = buffer.getNumChannels();
+    for (int chan = 0; chan < nChannels; ++chan)
+    {
+        const int nSamples = getNumSamples (chan);
+        float* bufPtr = buffer.getWritePointer (chan);
+        for (int n = 0; n < nSamples; ++n)
+        {
+          if (*(bufPtr + n) > highThreshold) {
+            counting = true;
+	    sampleCount = 0;
+          }
+	  else if (*(bufPtr + n) > lowThreshold) {
+	    *(bufPtr + n) = float(sampleCount)/100.0;
+	    //std::cout << "sampleCount: " << sampleCount << std::endl;
+	  }
+          else {
+	    ++sampleCount;
+            *(bufPtr + n) = 0.0;
+	    if (sampleCount > timeOut) {
+	      counting = false;
+	      sampleCount = 0;
+	    }
+          }
+        }
+
+        /* =============================================================================
+          Do something here.
+
+          To obtain a read-only pointer to the n sample of a channel:
+          float* samplePtr = buffer.getReadPointer(chan,n);
+
+          To obtain a read-write pointer to the n sample of a channel:
+          float* samplePtr = buffer.getWritePointer(chan,n);
+
+          All the samples in a channel are consecutive, so this example is valid:
+          float* samplePtr = buffer.getWritePointer(chan,0);
+          for (i=0; i < nSamples; i++)
+          {
+         *(samplePtr+i) = (*samplePtr+i)+offset;
+         }
+
+         See also documentation and examples for buffer.copyFrom and buffer.addFrom to operate on entire channels at once.
+
+         To add a TTL event generated on the n-th sample:
+         addEvents(events, TTL, n);
+         =============================================================================== */
+    }
+
+    /** Simple example that creates an event when the first channel goes under a negative threshold
+
+      for (int i = 0; i < getNumSamples(channels[0]->sourceNodeId); i++)
+      {
+      if ((*buffer.getReadPointer(0, i) < -threshold) && !state)
+      {
+
+    // generate midi event
+    addEvent(events, TTL, i);
+
+    state = true;
+
+    } else if ((*buffer.getReadPointer(0, i) > -threshold + bufferZone)  && state)
+    {
+    state = false;
+    }
+
+    }
+    */
+}
+
+
+void LatencyThresholdProcessor::saveCustomParametersToXml (XmlElement* parentElement)
+{
+    XmlElement* mainNode = parentElement->createNewChildElement ("LatencyThresholdProcessor");
+    mainNode->setAttribute ("numParameters", getNumParameters());
+
+    // Open Ephys Plugin Generator will insert generated code to save parameters here. Don't edit this section.
+    //[OPENEPHYS_PARAMETERS_SAVE_SECTION_BEGIN]
+    for (int i = 0; i < getNumParameters(); ++i)
+    {
+        XmlElement* parameterNode = mainNode->createNewChildElement ("Parameter");
+
+        auto parameter = getParameterObject(i);
+        parameterNode->setAttribute ("name", parameter->getName());
+        parameterNode->setAttribute ("type", parameter->getParameterTypeString());
+
+        auto parameterValue = getParameterVar (i, currentChannel);
+
+        if (parameter->isBoolean())
+            parameterNode->setAttribute ("value", (int)parameterValue);
+        else if (parameter->isContinuous() || parameter->isDiscrete() || parameter->isNumerical())
+            parameterNode->setAttribute ("value", (double)parameterValue);
+    }
+    //[OPENEPHYS_PARAMETERS_SAVE_SECTION_END]
+}
+
+
+void LatencyThresholdProcessor::loadCustomParametersFromXml()
+{
+    if (parametersAsXml == nullptr) // prevent double-loading
+        return;
+
+    // use parametersAsXml to restore state
+
+    // Open Ephys Plugin Generator will insert generated code to load parameters here. Don't edit this section.
+    //[OPENEPHYS_PARAMETERS_LOAD_SECTION_BEGIN]
+    forEachXmlChildElement (*parametersAsXml, mainNode)
+    {
+        if (mainNode->hasTagName ("LatencyThresholdProcessor"))
+        {
+            int parameterIdx = -1;
+
+            forEachXmlChildElement (*mainNode, parameterNode)
+            {
+                if (parameterNode->hasTagName ("Parameter"))
+                {
+                    ++parameterIdx;
+
+                    String parameterType = parameterNode->getStringAttribute ("type");
+                    if (parameterType == "Boolean")
+                        setParameter (parameterIdx, parameterNode->getBoolAttribute ("value"));
+                    else if (parameterType == "Continuous" || parameterType == "Numerical")
+                        setParameter (parameterIdx, parameterNode->getDoubleAttribute ("value"));
+                    else if (parameterType == "Discrete")
+                        setParameter (parameterIdx, parameterNode->getIntAttribute ("value"));
+                }
+            }
+        }
+    }
+    //[OPENEPHYS_PARAMETERS_LOAD_SECTION_END]
+}
